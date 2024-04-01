7df03a6585e612a7a53342d61bb786d35688a6de
Daniil <felix.trof@gmail.com>
2024-04-01 10:58:00 +0300

    chats fixed attempt 1

--- deal/settings.py
+++ deal/settings.py
@@ -36,13 +36,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'chats',
     'core',
     'storage',
     'rest_framework',
     'rest_framework_simplejwt.token_blacklist',
     'corsheaders',
     'users',
-    'chats'
 ]
 
 AUTH_USER_MODEL = 'users.DealUser'
