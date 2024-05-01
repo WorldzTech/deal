97f6a43fb4f08fc0120c8d11f3715c79b8515ff5
Daniil Baykalov <felix.trof@gmail.com>
2024-05-01 15:58:37 +0300

    product tag length increase

--- core/models.py
+++ core/models.py
@@ -27,7 +27,7 @@ class ProductTagGroup(models.Model):
 
 
 class ProductTag(models.Model):
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
