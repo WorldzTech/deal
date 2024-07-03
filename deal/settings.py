be73333aa094f99e41ffa311122ade476b5e23ed
Daniil Baykalov <felix.trof@gmail.com>
2024-07-04 02:25:52 +0300

    cors whitelist 2

--- deal/settings.py
+++ deal/settings.py
@@ -58,23 +58,45 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
-    'http://192.168.10.8:3000',
-    'http://127.0.0.1:3000',
-    'https://deal-fashion.com',
-    'https://admin.deal-fashion.com'
-]
-
-CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000',
-    'http://192.168.10.8:3000',
-    'http://127.0.0.1:3000',
-    'https://deal-fashion.com',
-    'https://admin.deal-fashion.com'
-]
+# CORS_ALLOWED_ORIGINS = [
+#     'http://localhost:3000',
+#     'http://192.168.10.8:3000',
+#     'http://127.0.0.1:3000',
+#     'https://deal-fashion.com',
+#     'https://admin.deal-fashion.com'
+# ]
+#
+# CORS_ORIGIN_WHITELIST = [
+#     'http://localhost:3000',
+#     'http://192.168.10.8:3000',
+#     'http://127.0.0.1:3000',
+#     'https://deal-fashion.com',
+#     'https://admin.deal-fashion.com'
+# ]
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
