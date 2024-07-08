b9e4af4bdc4b82a9ecb149312d88025564046aa1
Daniil <felix.trof@gmail.com>
2024-07-08 17:03:42 +0300

    size max length increased to 9

--- storage/models.py
+++ storage/models.py
@@ -6,5 +6,5 @@ from core.models import Product
 # Create your models here.
 class StorageUnit(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='product')
-    size = models.CharField(max_length=5)
+    size = models.CharField(max_length=9)
     amount = models.IntegerField()
