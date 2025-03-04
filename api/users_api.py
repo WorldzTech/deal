7f142f89ac343be2e1ea06ec160b3448cefb0c69
daniilbaikalov <felix.trof@gmail.com>
2025-03-04 12:58:58 +0300

    admin users endpoint

--- api/users_api.py
+++ api/users_api.py
@@ -1,12 +1,17 @@
 from rest_framework import status
+from rest_framework.permissions import IsAdminUser
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from api.serializers import UserSerializer
 from users.models import DealUser
 
 
 class GetAllUsers(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAdminUser]
+
     def get(self, request):
         users = DealUser.objects.all()
 
