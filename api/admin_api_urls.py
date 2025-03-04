ba3f1ba00be99d634e724fe806e6c14ce2115af1
daniilbaikalov <felix.trof@gmail.com>
2025-03-04 14:05:08 +0300

    editable images

--- api/admin_api_urls.py
+++ api/admin_api_urls.py
@@ -41,5 +41,5 @@ urlpatterns = [
     path('storage/unit/adjust/', StoragePositionAdjust.as_view(), name='admin_storage_adjust'),
     path('storage/unit/remove/', RemoveStorageUnit.as_view(), name='admin_storage_unit_remove'),
     path('users/all', GetAllUsers.as_view(), name='admin_users_all'),
-    path('editableImages/', GetEditableImage.as_view(), name='admin_editable_images'),
+    path('editableImages/', EditableImages.as_view(), name='admin_editable_images'),
 ]
